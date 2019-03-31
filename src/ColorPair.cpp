a92d566cb3220aa2e7d526d61d125d9dac49de54
GOKOP <igorbugajski0@spoko.pl>
2019-03-31 23:16:25 +0200

    removed sth that shouldn't have been comitted

--- src/ColorPair.cpp
+++ src/ColorPair.cpp
@@ -7,15 +7,11 @@ ColorPair::ColorPair(int num, int fground, int bground, bool is_bold) {
 }
 
 void ColorPair::enable(WINDOW* win) {
-	if(pair_num>=0) {
-		wattron(win, COLOR_PAIR(pair_num));
-		if(bold) wattron(win, A_BOLD);
-	}
+	wattron(win, COLOR_PAIR(pair_num));
+	if(bold) wattron(win, A_BOLD);
 }
 
 void ColorPair::disable(WINDOW* win) {
-	if(pair_num>=0) {
-		wattroff(win, COLOR_PAIR(pair_num));
-		if(bold) wattroff(win, A_BOLD);
-	}
+	wattroff(win, COLOR_PAIR(pair_num));
+	if(bold) wattroff(win, A_BOLD);
 }
